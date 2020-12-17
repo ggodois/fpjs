7955015d19ebba1f2ba574d0ac7f0535f84b97de
Gabriel Godoi Silva <gabriel.silva@midway.com.br>
2020-12-17 11:45:16 -0300

    delete flashcard_completed and finishes my own

--- flashcard/src/Update.js
+++ flashcard/src/Update.js
@@ -4,12 +4,124 @@ const MSGS = {
   DELETE_CARD: 'DELETE_CARD',
   SHOW_ANSWER: 'SHOW_ANSWER',
   GRADE_ANSWER: 'GRADE_ANSWER',
+  QUESTION_INPUT: 'QUESTION_INPUT',
+  ANSWER_INPUT: 'ANSWER_INPUT',
+  SAVE: 'SAVE',
+};
+export const RANKS = {
+  BAD: 0,
+  GOOD: 1,
+  GREAT: 2,
 };
 
+export const showAnswerMessage = id => ({
+  type: MSGS.SHOW_ANSWER,
+  id,
+});
 
+export const gradeAnswerMessage = (id, rank) => ({
+  type: MSGS.GRADE_ANSWER,
+  id,
+  rank,
+});
+
+export const editCardMessage = id => ({
+  type: MSGS.EDIT_CARD,
+  id,
+});
+
+export const deleteCardMessage = id => ({
+  type: MSGS.DELETE_CARD,
+  id,
+});
+
+export const questionInputMessage = (id, question) => ({
+  type:MSGS.QUESTION_INPUT,
+  id,
+  question,
+});
+
+export const answerInputMessage = (id, answer) => ({
+  type:MSGS.ANSWER_INPUT,
+  answer,
+  id,
+});
+
+export const saveMessage = id => ({
+  type: MSGS.SAVE,
+  id,
+});
+
+export const addCardMessage = { type: MSGS.ADD_CARD };
+
+const updateCards = (newDataCard, cards) => cards.map(card => {
+  if (card.id === newDataCard.id) return { ...card, ...newDataCard };
+  return card
+});
 
 function update(msg, model) {
-  return model;
+  const { cards, nextId } = model;
+  switch (msg.type) {
+    case MSGS.SHOW_ANSWER: {
+      const { id } = msg;
+      const updatedCards = updateCards({ id, hiddenAnswer: false }, cards);
+      return { ...model, cards: updatedCards };
+    }
+    case MSGS.QUESTION_INPUT: {
+      const { id, question } = msg;
+      const updatedCards = updateCards({ id, question }, cards);
+      return { ...model, cards: updatedCards };
+    }
+    case MSGS.ANSWER_INPUT: {
+      const { id, answer } = msg;
+      const updatedCards = updateCards({ id, answer }, cards);
+      return { ...model, cards: updatedCards };
+    }
+    case MSGS.SAVE: {
+      const { id } = msg;
+      const updatedCards = updateCards({ id, edit: false }, cards);
+      return { ...model, cards: updatedCards };
+    }
+    case MSGS.DELETE_CARD: {
+      const { id } = msg;
+      const updatedCards = cards.filter(card => card.id !== id);
+      return { ...model, cards: updatedCards };
+    }
+    case MSGS.EDIT_CARD: {
+      const { id } = msg;
+      const updatedCards = updateCards({ id, edit: true }, cards);
+      return { ...model, cards: updatedCards };
+    }
+    case MSGS.GRADE_ANSWER: {
+      const { id, rank } = msg;
+      const card = cards.find(e => e.id === id);
+      let grade;
+      if (rank === RANKS.BAD) grade = 0;
+      else if (rank === RANKS.GOOD) grade = card.rank + 1;
+      else if (rank === RANKS.GREAT) grade = card.rank + 2;
+      const updatedCards = updateCards({ id, hiddenAnswer: true, rank: grade }, cards)
+        .sort((a, b) => {
+          if (a.rank < b.rank && a.id < b.id) return -1;
+          if (a.rank > b.rank && a.id > b.id) return 1;
+          return 0;
+        });
+      return { ...model, cards: updatedCards };
+    }
+    case MSGS.ADD_CARD: {
+      const newCard = {
+        id: nextId,
+        question: '',
+        answer: '',
+        rank: 0,
+        showAnswer: false,
+        edit: true,
+      };
+      cards.unshift(newCard);
+      return { ...model, cards, nextId: nextId + 1 };
+    }
+    default:
+      return model;
+  }
 }
 
 export default update;
--- flashcard/src/View.js
+++ flashcard/src/View.js
@@ -1,94 +1,145 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
+import {
+  addCardMessage,
+  answerInputMessage,
+  deleteCardMessage,
+  editCardMessage,
+  gradeAnswerMessage,
+  questionInputMessage,
+  RANKS,
+  saveMessage,
+  showAnswerMessage,
+} from './Update';
+
 const {
-  button, div, h1, i, pre, textarea,
+  a, button, div, h1, i, pre, textarea,
 } = hh(h);
 
-const addButton = (className, type, options) => div([
-  button({ className, type }, options),
+const addButton = ({
+  className, onclick, options,
+}) => div([
+  button({ className, onclick }, options),
 ]);
 
-const addFlashCardBtn = () => {
-  return addButton(
-    'pa2 br1 mv2 bg-green bn white',
-    'submit',
-    [i({ className: 'fa fa-plus ph1' }), 'Add Flashcard'],
-  );
+const addFlashCardBtn = (dispatch) => {
+  return addButton({
+    className: 'pa2 br1 mv2 bg-green bn white',
+    onclick: () => dispatch(addCardMessage),
+    options: [i({ className: 'fa fa-plus ph1' }), 'Add Flashcard'],
+  });
 };
 
-const cardSection = (
-  titleClassName, title, bodyTag, bodyClassName, body,
-) => div([
-  div({ className: titleClassName }, title),
-  bodyTag({ className: bodyClassName }, body),
+const removeIcon = (dispatch, card) => i({
+  className: 'absolute top-0 right-0 fa fa-remove fa-fw black-50 pointer',
+  onclick: () => dispatch(deleteCardMessage(card.id)),
+});
+
+const editQuestion = (dispatch, card) => div({ className: '' }, [
+  div({ className: 'b f6 mv1' }, 'Question'),
+  textarea({
+    className: 'w-100 bg-washed-yellow outline-0 h4',
+    value: card.question,
+    oninput: e => dispatch(questionInputMessage(card.id, e.target.value)),
+  }),
 ]);
 
-const selectedCard = ({ question, answer }) => {
-  return div({ className: 'w-third pa2' }, [
-    div({ className: 'w-100 pa2 bg-light-yellow shadow-1 mv2 relative pb5' }, [
-      cardSection(
-        'b f6 mv1 underline', 'Question', div, 'pointer', question,
-      ),
-      cardSection(
-        'b f6 mv1 underline', 'Answer', div, 'pointer', answer,
-      ),
-      div({ className: 'absolute bottom-0 left-0 w-100 ph2' }, [
-        div({ className: 'mv2 flex justify-between' }, [
-          addButton('f4 ph3 pv2 bg-red bn white br1', 'submit', 'Bad'),
-          addButton('f4 ph3 pv2 bg-blue bn white br1', 'submit', 'Good'),
-          addButton('f4 ph3 pv2 bg-dark-green bn white br1', 'submit', 'Great'),
-        ]),
-      ]),
-      i({ className: 'absolute top-0 right-0 fa fa-remove fa-fw black-50 pointer' }),
-    ]),
-  ]);
-};
+const editAnswer = (dispatch, card) => div({ className: '' }, [
+  div({ className: 'b f6 mv1' }, 'Answer'),
+  textarea({
+    className: 'w-100 bg-washed-yellow outline-0 h4',
+    value: card.answer,
+    oninput: e => dispatch(answerInputMessage(card.id, e.target.value)),
+  }),
+]);
 
-const editCard = () => {
+const editCard = (dispatch, card) => {
   return div({ className: 'w-third pa2' }, [
     div({ className: 'w-100 pa2 bg-light-yellow shadow-1 mv2 relative' }, [
-      cardSection(
-        'b f6 mv1', 'Question', textarea, 'w-100 bg-washed-yellow outline-0', null,
-      ),
-      cardSection(
-        'b f6 mv1', 'Answer', textarea, 'w-100 bg-washed-yellow outline-0', null,
-      ),
-      addButton('f4 ph3 pv2 br1 bg-gray bn white mv2', 'submit', 'Save'),
-      i({ className: 'absolute top-0 right-0 fa fa-remove fa-fw black-50 pointer' }),
+      editQuestion(dispatch, card),
+      editAnswer(dispatch, card),
+      addButton({
+        className: 'f4 ph3 pv2 br1 bg-gray bn white mv2',
+        onclick: () => dispatch(saveMessage(card.id)),
+        options: 'Save',
+      }),
+      removeIcon(dispatch, card),
     ]),
   ]);
 };
 
-const card = ({ question, answer }) => {
+const viewQuestion = (dispatch, card) => div([
+  div({ className: 'b f6 mv1 underline ph1' }, 'Question'),
+  div(
+    {
+      className: 'pointer hover-bg-black-10  bg-animate pv2 ph',
+      onclick: () => dispatch(editCardMessage(card.id)),
+    },
+    card.question,
+  ),
+]);
+
+const viewAnswer = (dispatch, card) => card.hiddenAnswer
+  ? div(a(
+    {
+      className: 'f6 underline link pointer',
+      onclick: () => dispatch(showAnswerMessage(card.id)),
+    },
+    'Show Answer',
+  ))
+  : div([
+    div({ className: 'b f6 mv1 underline ph1' }, 'Answer'),
+    div(
+      {
+        className: 'pointer hover-bg-black-10 bg-animate pv2 ph1',
+        onclick: () => dispatch(editCardMessage(card.id)),
+      },
+      card.answer,
+    ),
+  ]);
+
+const viewRankButtons = (dispatch, card) => card.hiddenAnswer
+  ? null
+  : div({ className: 'absolute bottom-0 left-0 w-100 ph2' }, [
+    div({ className: 'mv2 flex justify-between' }, [
+      addButton({
+        className: 'f4 ph3 pv2 bg-red bn white br1',
+        onclick: () => dispatch(gradeAnswerMessage(card.id, RANKS.BAD)),
+        options: 'Bad',
+      }),
+      addButton({
+        className: 'f4 ph3 pv2 bg-blue bn white br1',
+        onclick: () => dispatch(gradeAnswerMessage(card.id, RANKS.GOOD)),
+        options: 'Good',
+      }),
+      addButton({
+        className: 'f4 ph3 pv2 bg-dark-green bn white br1',
+        onclick: () => dispatch(gradeAnswerMessage(card.id, RANKS.GREAT)),
+        options: 'Great',
+      }),
+    ]),
+  ]);
+
+const viewCard = (dispatch, card) => {
   return div({ className: 'w-third pa2' }, [
     div({ className: 'w-100 pa2 bg-light-yellow shadow-1 mv2 relative pb5' }, [
-      cardSection(
-        'b f6 mv1 underline', 'Question', div, 'pointer', question,
-      ),
-      cardSection(
-        'f6 mv1 underline', 'Show Answer', div, 'pointer', null,
-      ),
-      i({ className: 'absolute top-0 right-0 fa fa-remove fa-fw black-50 pointer' }),
+      viewQuestion(dispatch, card),
+      viewAnswer(dispatch, card),
+      viewRankButtons(dispatch, card),
+      removeIcon(dispatch, card),
     ]),
   ]);
 };
 
-const cardsSection = (dispatch, model) => {
-  return div({ className: 'flex flex-wrap nl2 nr2' }, [
-    // model.cards.map(item => card(item)),
-    selectedCard(model.cards[0]),
-    editCard(),
-    card(model.cards[0]),
-    card(model.cards[0]),
-  ]);
-};
+const cardsSection = (dispatch, card) => card.edit ? editCard(dispatch, card) : viewCard(dispatch, card);
 
 function view(dispatch, model) {
+  const cards = model.cards.map(card => cardsSection(dispatch, card));
   return div({ className: 'mw8 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Flashcard Study'),
-    addFlashCardBtn(),
-    cardsSection(dispatch, model),
+    addFlashCardBtn(dispatch),
+    div({ className: 'flex flex-wrap nl2 nr2' }, cards),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
